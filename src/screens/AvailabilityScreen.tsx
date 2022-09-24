462d6d30671701d08690a94b8949e0be6c1e9144
William <NkunaTebogo09@gmail.com>
2022-09-24 22:52:27 +0200

    Added all required screens

---
+++ src/screens/AvailabilityScreen.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const AvailabilityScreen = () => {
+  return (
+    <View>
+      <Text>AvailabilityScreen</Text>
+    </View>
+  );
+};
+
+export default AvailabilityScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/CaptureImageScreen.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const CaptureImageScreen = () => {
+  return (
+    <View>
+      <Text>CaptureImageScreen</Text>
+    </View>
+  );
+};
+
+export default CaptureImageScreen;
+
+const styles = StyleSheet.create({});
