ab94d3c6d9ebdb9dfb4f5c02eaa4de2e1a25f8d2
Nkuna William <64930245+NK-William@users.noreply.github.com>
2022-10-18 23:10:08 +0200

    Updates on availability page

---
+++ src/components/DayBubble.tsx
@@ -0,0 +1,68 @@
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React from "react";
+import { LinearGradient } from "react-native-linear-gradient";
+import { PrimaryColor, AccentColor } from "../resources/colors";
+import Icon  from "react-native-vector-icons/Ionicons";
+
+interface bubbleType {
+    bubbleRightColor: string,
+    day: string,
+    dayRadioBtnName: string,
+    setRadioButtons:  () => void 
+}
+
+const DayBubble = ({
+  bubbleRightColor,
+  day,
+  dayRadioBtnName,
+  setRadioButtons,
+} : bubbleType) => {
+  return (
+    <LinearGradient
+      style={styles.dayBubble}
+      colors={[PrimaryColor, bubbleRightColor]}
+      start={{ x: 0.4, y: 0.4 }}
+      end={{ x: 1, y: 1 }}
+    >
+      <Text style={styles.dayText}>{day}</Text>
+      <View style={styles.weeklySelectView}>
+        <Text style={styles.weeklyText}>Weekly</Text>
+        <TouchableOpacity onPress={() => setRadioButtons()}>
+          <Icon name={dayRadioBtnName} size={16} color={AccentColor} />
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
+  );
+};
+
+export default DayBubble;
+
+const styles = StyleSheet.create({
+  dayBubble: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 100,
+    width: 150,
+    borderRadius: 50,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  dayText: { color: "white", fontSize: 18 },
+  weeklySelectView: {
+    flexDirection: "row",
+    position: "absolute",
+    bottom: 0,
+    alignItems: "center",
+    marginBottom: 4,
+  },
+  weeklyText: {
+    color: "white",
+    fontSize: 10,
+    marginRight: 2,
+  },
+});
--- src/screens/EmployeeAvailabilityScreen.tsx
+++ src/screens/EmployeeAvailabilityScreen.tsx
@@ -1,14 +1,160 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { PrimaryColor } from "../resources/colors";
+import DayBubble from "../components/DayBubble";
 
 const EmployeeAvailabilityScreen = () => {
+  const [radioButtons, setRadioButtons] = useState({
+    mondayWeekly: false,
+    tuesdayWeekly: false,
+    wednesdayWeekly: false,
+    thursdayWeekly: false,
+    fridayWeekly: false,
+    saturdayWeekly: false,
+    sundayWeekly: false,
+  });
+
+  const mondayRadioBtnName = radioButtons.mondayWeekly
+    ? "radio-button-on"
+    : "radio-button-off";
+  const tuesdayRadioBtnName = radioButtons.tuesdayWeekly
+    ? "radio-button-on"
+    : "radio-button-off";
+  const wednesdayRadioBtnName = radioButtons.wednesdayWeekly
+    ? "radio-button-on"
+    : "radio-button-off";
+  const thursdayRadioBtnName = radioButtons.thursdayWeekly
+    ? "radio-button-on"
+    : "radio-button-off";
+  const fridayRadioBtnName = radioButtons.fridayWeekly
+    ? "radio-button-on"
+    : "radio-button-off";
+  const saturdayRadioBtnName = radioButtons.saturdayWeekly
+    ? "radio-button-on"
+    : "radio-button-off";
+  const sundayRadioBtnName = radioButtons.sundayWeekly
+    ? "radio-button-on"
+    : "radio-button-off";
+
   return (
-    <View>
-      <Text>EmployeeAvailabilityScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Choose your availability</Text>
+      <View style={styles.gridView}>
+        <View style={styles.gridRowView}>
+          <DayBubble
+            bubbleRightColor="yellow"
+            day="Monday"
+            dayRadioBtnName={mondayRadioBtnName}
+            setRadioButtons={() =>
+              setRadioButtons({
+                ...radioButtons,
+                mondayWeekly: !radioButtons.mondayWeekly,
+              })
+            }
+          />
+          <View style={styles.space} />
+          <DayBubble
+            bubbleRightColor="pink"
+            day="Tuesday"
+            dayRadioBtnName={tuesdayRadioBtnName}
+            setRadioButtons={() =>
+              setRadioButtons({
+                ...radioButtons,
+                tuesdayWeekly: !radioButtons.tuesdayWeekly,
+              })
+            }
+          />
+        </View>
+        <View style={styles.gridRowView}>
+          <DayBubble
+            bubbleRightColor="green"
+            day="Wednesday"
+            dayRadioBtnName={wednesdayRadioBtnName}
+            setRadioButtons={() =>
+              setRadioButtons({
+                ...radioButtons,
+                wednesdayWeekly: !radioButtons.wednesdayWeekly,
+              })
+            }
+          />
+          <View style={styles.space} />
+          <DayBubble
+            bubbleRightColor="orange"
+            day="Thursday"
+            dayRadioBtnName={thursdayRadioBtnName}
+            setRadioButtons={() =>
+              setRadioButtons({
+                ...radioButtons,
+                thursdayWeekly: !radioButtons.thursdayWeekly,
+              })
+            }
+          />
+        </View>
+        <View style={styles.gridRowView}>
+          <DayBubble
+            bubbleRightColor="blue"
+            day="Friday"
+            dayRadioBtnName={fridayRadioBtnName}
+            setRadioButtons={() =>
+              setRadioButtons({
+                ...radioButtons,
+                fridayWeekly: !radioButtons.fridayWeekly,
+              })
+            }
+          />
+          <View style={styles.space} />
+          <DayBubble
+            bubbleRightColor="purple"
+            day="Saturday"
+            dayRadioBtnName={saturdayRadioBtnName}
+            setRadioButtons={() =>
+              setRadioButtons({
+                ...radioButtons,
+                saturdayWeekly: !radioButtons.saturdayWeekly,
+              })
+            }
+          />
+        </View>
+        <View style={styles.gridRowView}>
+          <DayBubble
+            bubbleRightColor="red"
+            day="Sunday"
+            dayRadioBtnName={sundayRadioBtnName}
+            setRadioButtons={() =>
+              setRadioButtons({
+                ...radioButtons,
+                sundayWeekly: !radioButtons.sundayWeekly,
+              })
+            }
+          />
+        </View>
+      </View>
     </View>
   );
 };
 
 export default EmployeeAvailabilityScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  titleText: {
+    alignSelf: "center",
+    color: PrimaryColor,
+    fontWeight: "bold",
+    margin: 8,
+    fontSize: 16,
+  },
+  space: { marginHorizontal: 8 },
+  container: { flex: 1 },
+  gridRowView: {
+    flexDirection: "row",
+    marginVertical: 8,
+  },
+  radioButton: {},
+  gridView: {
+    marginHorizontal: 8,
+    marginBottom: 18,
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+});
