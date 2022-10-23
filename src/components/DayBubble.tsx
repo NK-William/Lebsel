81f4053c9313d746d967e2c73486aa5a70f9589e
Nkuna William <64930245+NK-William@users.noreply.github.com>
2022-10-23 20:13:09 +0200

    Fix on "DayBubble" component.

--- src/components/DayBubble.tsx
+++ src/components/DayBubble.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
-import { LinearGradient } from "react-native-linear-gradient";
+import LinearGradient from "react-native-linear-gradient";
 import { PrimaryColor, AccentColor } from "../resources/colors";
 import Icon  from "react-native-vector-icons/Ionicons";
 
