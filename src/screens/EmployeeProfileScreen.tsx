80e67aef2c1cbc956925923917a3d218ff7ebbfd
Nkuna William <64930245+NK-William@users.noreply.github.com>
2022-10-24 10:58:01 +0200

    Code push with minor issue

--- src/screens/EmployeeProfileScreen.tsx
+++ src/screens/EmployeeProfileScreen.tsx
@@ -1,14 +1,35 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, Text, View, Button, Image} from 'react-native';
+import React, { useState } from 'react';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 const EmployeeProfileScreen = () => {
+  const [image, setImage] = useState<any | undefined>();
+
+  const btnClicked = async () => {
+    console.log("Pressed")
+     const result = await launchCamera({mediaType: 'photo'});
+    //const result = await launchImageLibrary({mediaType: 'photo'});
+    let imageObject : any = result.assets[0];
+    console.log(imageObject)
+    console.log("uri *****", imageObject.uri)
+    setImage(result);
+  }
+
   return (
-    <View>
-      <Text>EmployeeProfileScreen</Text>
+    <View style={{flex: 1}}>
+      <Button title="Click" onPress={() => btnClicked()}/>
+      <Image
+          source={
+            image ? { uri: image.uri } : require("../resources/images/profilepicplaceholder.png")
+          }
+        />
     </View>
+    
   );
 };
 
 export default EmployeeProfileScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+}); 
+
